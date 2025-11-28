b4de267db3a4bd958750860037c0e723878cb4a9
Marc Wijesuriya <marcwijesuriya@gmail.com>
2025-11-28 14:11:44 +0530

    Update backend.tf
    Changed region from a variable to 'us-east-1'

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state"
     key            = "global/terraform.tfstate"
-    region         = var.region
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
