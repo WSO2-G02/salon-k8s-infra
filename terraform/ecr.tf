e790e854bc54512c94eb7135c2a552c8e19326c8
Ravindu Dilhan <dilhanw-ct21062@stu.kln.ac.lk>
2025-12-03 10:27:48 +0530

    Updated ecr.tf file to create ECR repositories

---
+++ terraform/ecr.tf
@@ -0,0 +1,44 @@
+variable "project_tag" {
+  type    = string
+  default = "salon-booking-system"
+}
+
+locals {
+  services = [
+    "user_service",
+    "appointment_service",
+    "service_managemnet",
+    "staff_management",
+    "notification_service",
+    "reports_analytics",
+    "frontend"
+  ]
+}
+
+resource "aws_ecr_repository" "repos" {
+  for_each = toset(local.services)
+
+  name                 = each.key
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Project = var.project_tag
+    Service = each.key
+  }
+}
+
+output "ecr_urls" {
+  value = {
+    for k, r in aws_ecr_repository.repos :
+    k => r.repository_url
+  }
+}
+
