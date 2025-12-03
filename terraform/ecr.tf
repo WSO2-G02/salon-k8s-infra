93f7a1e7d4b40fd003242c95132412ba4d2beeeb
Ravindu Dilhan <dilhanw-ct21062@stu.kln.ac.lk>
2025-12-03 10:45:09 +0530

    Changed ecr.tf file

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 variable "project_tag" {
   type    = string
   default = "salon-booking-system"
@@ -40,16 +39,6 @@ output "ecr_urls" {
   value = {
     for k, r in aws_ecr_repository.repos :
     k => r.repository_url
+  
   }
 }
-
-=======
-# ECR repository
-resource "aws_ecr_repository" "app" {
-  name                 = var.project_name
-  image_tag_mutability = "MUTABLE"
-  encryption_configuration {
-    encryption_type = "AES256"
-  }
-}
->>>>>>> 6fd1020d163047f3cd4852ece8531b3b2b1a62c8
