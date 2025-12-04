ac7d9da6992b526b63587f404efd1bdee3dbcbb7
Marc Wijesuriya <marcwijesuriya@gmail.com>
2025-12-04 20:43:29 +0530

    Update backend.tf
    Changed the S3 bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "salon-terraform-state10249342"
+    bucket       = "salon-terraform-state10249343"
     key          = "global/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
