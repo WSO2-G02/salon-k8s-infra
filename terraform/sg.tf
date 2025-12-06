9d104368b7c87479baa074110eb7f4c544ef46eb
Marc Wijesuriya <marcwijesuriya@gmail.com>
2025-12-06 12:39:51 +0530

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -83,7 +83,7 @@ resource "aws_security_group" "ec2_sg" {
     description = "BGP routing (Calico or other)"
   }
 
-  # HTTP Traffic from LoadBalancer a
+  # HTTP Traffic from LoadBalancer
 
   ingress {
     from_port       = 80
