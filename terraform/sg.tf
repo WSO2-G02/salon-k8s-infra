778b18bdd803fe8eb9376b7efb9798f40292f6e2
Marc Wijesuriya <marcwijesuriya@gmail.com>
2025-12-06 12:31:58 +0530

    updated workflow

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -93,7 +93,7 @@ resource "aws_security_group" "ec2_sg" {
     description     = "HTTP traffic from LoadBalancer"
   }
 
-  # Allow all outbound
+  # Allow all Outbound
 
   egress {
     from_port   = 0
