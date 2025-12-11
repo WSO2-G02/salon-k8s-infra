1495d63d89b87dd3a9e85649546828ad08e03190
Marc Wijesuriya <marcwijesuriya@gmail.com>
2025-12-11 12:06:18 +0530

    Terraform formatted asg_nodes.tf

--- terraform/asg_nodes.tf
+++ terraform/asg_nodes.tf
@@ -6,7 +6,7 @@ data "aws_autoscaling_group" "k8s" {
 # Get EC2 instances belonging to the ASG
 data "aws_instances" "k8s_nodes" {
   instance_tags = {
-    Role = "k8s-node"
+    Role    = "k8s-node"
     Project = var.project_tag
   }
   instance_state_names = ["running"]
